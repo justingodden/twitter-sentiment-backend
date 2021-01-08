9ebda97a2184ea54e0a612e58c8395a421407852
justingodden <justingodden@gmail.com>
2021-01-08 15:46:45 +0000

    API fully working!

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
 # source twitter-backend/Scripts/activate
+
+import json
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from twitter import twitter_search
+
+
+app = Flask(__name__)
+
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@app.route('/', methods=['POST'])
+def index():
+    if request.is_json:
+        req = request.get_json()
+        json_data = twitter_search(str(req['search']))
+        json_data = json.dumps(json_data)
+        print(json_data)
+        return json_data
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000, debug=True)
