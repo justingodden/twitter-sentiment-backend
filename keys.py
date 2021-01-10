13365df13e2678c2e32da57f40303cd09ff2caee
justingodden <43709962+justingodden@users.noreply.github.com>
2021-01-10 16:22:47 +0000

    Delete keys.py

--- keys.py
+++
@@ -1,3 +0,0 @@
-api_key = "EbwInRqREHOT4yvJsMQJyyD5o"
-api_secret_key = "6UYIFWcwARCzv6VeYAWhiMtOAA0tQgATzqOaiujf6DeJVperEF"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAAlFLQEAAAAAcnpdfBhYq4ZVpsnZB7SCqzO0lqA%3Dius6apm3PePU1UTqZitucSVt0Wp6jifIrW6d6F86kX4N13jJen"
